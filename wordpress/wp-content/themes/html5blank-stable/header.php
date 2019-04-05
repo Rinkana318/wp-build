b8c1d8e805ea78561a594996271851be904537e6
Max Taylor <mtaylor31801@gmail.com>
2019-04-05 21:09:11 +0000

    final commit

--- wordpress/wp-content/themes/html5blank-stable/header.php
+++ wordpress/wp-content/themes/html5blank-stable/header.php
@@ -31,14 +31,13 @@
 			<!-- header -->
 			<div class="header clearfix" >
 
-					<!-- logo -->
+					<!-- logo 
 					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+						<a href="<?php //echo home_url(); ?>">
+							<img src="<?php //echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
-					<!-- /logo -->
+					 /logo -->
 
 					<!-- nav -->
 					<nav>
